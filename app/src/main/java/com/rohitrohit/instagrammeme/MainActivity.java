7111d1dde2001c0d611c7fe96b9db82ccd811d56
ROHIT SANJAY GANDE <rohitgande0705@gmail.com>
2021-08-21 13:16:52 +0530

    Ui is ready

--- app/src/main/java/com/rohitrohit/instagrammeme/MainActivity.java
+++ app/src/main/java/com/rohitrohit/instagrammeme/MainActivity.java
@@ -5,61 +5,15 @@ import static android.content.ContentValues.TAG;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.QuickContactBadge;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.bumptech.glide.Glide;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    String url = "https://meme-api.herokuapp.com/gimme";
-    ImageView imageView;
-    Button refBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageV);
-        refBtn = findViewById(R.id.refrashbtn);
-        refBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getmeme();
-            }
-        });
-        getmeme();
 
     }
-
-    private void getmeme() {
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                response -> {
-                    try {
-                        String imagurl = response.getString("url");
-                        Glide.with(MainActivity.this).load(imagurl).into(imageView);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }, error -> Toast.makeText(MainActivity.this, "Error bhai", Toast.LENGTH_SHORT).show());
-
-        queue.add(jsonObjectRequest);
-    }
-
 }
